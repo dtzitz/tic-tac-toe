613d481add0df903102dc4bd98df2b1460343442
Dylan <dtzitz@live.com>
2016-10-09 10:29:21 -0400

    updates to the gameboard

--- app/components/game-board.js
+++ app/components/game-board.js
@@ -1,4 +1,34 @@
 import Ember from 'ember'
 
 export default Ember.Component.extend({
+    gameWon: false,
+    gamePiece:'',
+    
+    topLeft:'',
+    topCenter:'',
+    topRight:'',
+    middleLeft:'',
+    center:'',
+    middleRight:'',
+    bottomLeft:'',
+    bottomCenter:'',
+    bottomRight:'',
+
+    actions:{
+        buttonClicked(param){
+            this.set(param,'x')
+        },
+        newGame(param){
+            this.set('topLeft',''),
+            this.set('topCenter',''),
+            this.set('topRight',''),
+            this.set('middleLeft',''),
+            this.set('center',''),
+            this.set('middleRight',''),
+            this.set('bottomLeft',''),
+            this.set('bottomCenter',''),
+            this.set('bottomRight','')
+            $('.gameButtons').fadeToggle("slow","linear")
+        }
+    }
 })
